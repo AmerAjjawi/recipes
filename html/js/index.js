372788d45dc9f9d486ad5e4156fc4e519bdf5752
Amer Ajjawi <ajjawi_amer@hotmail.com>
2024-05-16 07:25:03 -0400

    created an assets folder to hold the images and laid out javascript file

---
+++ html/js/index.js
@@ -0,0 +1,5 @@
+//Image gallery function
+//1) setup the array images
+let images = ["https://assets.bonappetit.com/photos/622901c1eac42d2d741a3e19/1:1/w_320%2Cc_limit/BA0422peidmont09.jpg","https://assets.bonappetit.com/photos/63dadf5316330e47d621f630/16:9/w_320%2Cc_limit/0201023-spinach-ricotta-lasagna-lede.jpg", "https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_320%2Cc_limit/bolognese-1.jpg"];
+console.log(images);
+//2)loop through the array
