49592f40e37ae05ab38750ab5b2a7c20c076b57a
Amer Ajjawi <ajjawi_amer@hotmail.com>
2024-06-05 17:30:11 -0400

    deleted javascript laid out. Edited html lay out for the image carousel

--- html/js/index.js
+++ html/js/index.js
@@ -1,13 +1,2 @@
-//Image gallery function
-//1) setup the array images
-
-const ArrayOfImages = ["https://assets.bonappetit.com/photos/622901c1eac42d2d741a3e19/1:1/w_320%2Cc_limit/BA0422peidmont09.jpg","https://assets.bonappetit.com/photos/63dadf5316330e47d621f630/16:9/w_320%2Cc_limit/0201023-spinach-ricotta-lasagna-lede.jpg", "https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_320%2Cc_limit/bolognese-1.jpg"];
-console.log();
-//2)loop through the array
-ArrayOfImages.forEach(function(image) {
-    let img = document.getElementsByClassName("thumb-bar");
-    img.src = ArrayOfImages;
-    img.height = "50px";
-    img.width - "50px";
-    document.body.appendChild(ArrayOfImages);
-})
+//grab elements and create a variable for the images
+const 
