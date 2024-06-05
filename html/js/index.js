d83d7bb8751cee687b2bfa43e83d004b31809aed
Amer Ajjawi <ajjawi_amer@hotmail.com>
2024-06-05 17:45:02 -0400

    set a function to hide all images and only display one image at a time

--- html/js/index.js
+++ html/js/index.js
@@ -2,3 +2,13 @@
 const images = [...document.querySelectorAll(".displayed-img")];
 //initialize counter
 let counter = 0;
+
+//set a function to loop through images and hide them and show only a specific one
+function displaySlide(index) {
+    images.forEach(item => {
+        //hide all images
+        item.style.display = "none";
+    })
+    //display a specific image
+    images[index].style.display = "block";
+}
