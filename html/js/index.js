7a0924549162807db661c1d75b9131adf9824e86
Amer Ajjawi <ajjawi_amer@hotmail.com>
2024-06-11 19:47:58 -0400

    added setinterval to keep the page running and onload for the page to refresh

--- html/js/index.js
+++ html/js/index.js
@@ -1,29 +1,36 @@
-//grab elements
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    //grab elements
 const images = [...document.querySelectorAll(".slider")];
 console.log(images);
 let counter = 0;
 
 //set function to hide and display images
 function displaySlide(index) {
-    images.forEach(item => {
-        item.style.display = "none";
-    });
-    images[index].style.display = "block";
+  images.forEach(item => {
+    item.style.display = "none";
+  });
+  images[index].style.display = "block";
 }
 
 //function to move slide forward
 function moveSlide() {
-    counter = (counter + 1) % images.length;
-    displaySlide(counter);
+  counter = (counter + 1) % images.length;
+  displaySlide(counter);
 }
 
 //function to move slide back
 function moveSlideBack() {
-    counter = (counter - 1 + images.length) % images.length;
-    displaySlide(counter);
+  counter = (counter - 1 + images.length) % images.length;
+  displaySlide(counter);
 }
 
 //add button
 document.getElementById("forwardBtn").addEventListener("click", moveSlide);
 
 document.getElementById("backwardBtn").addEventListener("click", moveSlideBack);
+  
+  //automaticlly moving 
+setInterval(function() { moveSlide()}, 2000);
+
+});
